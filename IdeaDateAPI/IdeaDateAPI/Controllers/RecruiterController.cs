54455d2168e57fe29310f17d5e42b285c8bff24f
wjwalcher <wjwalcher@gmail.com>
2019-10-12 22:42:16 -0700

    Recruiter changes

--- IdeaDateAPI/IdeaDateAPI/Controllers/RecruiterController.cs
+++ IdeaDateAPI/IdeaDateAPI/Controllers/RecruiterController.cs
@@ -2,45 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdeaDateAPI.Models;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Newtonsoft.Json;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace IdeaDateAPI.Controllers
 {
     [Route("api/[controller]")]
     public class RecruiterController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+
+        private readonly IProjectRepository _projectRepository;
+        private readonly IUserRepository _userRepository;
+
+        public RecruiterController(IProjectRepository projectRepository,
+            IUserRepository userRepository)
         {
-            return new string[] { "value1", "value2" };
+            _projectRepository = projectRepository;
+            _userRepository = userRepository;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{uid}")]
+        public List<User> GetLikes(string uid)
         {
-            return "value";
+            List<string> results = new List<string>();
+            List<string> likes = (List<string>)
+                _projectRepository.GetProject(uid).Result.LikedBy;
+            List<User> users = (List<User>) _userRepository.GetUsers().Result
+                .Where(x => likes.Contains(x.UID));
+
+            return users;          
         }
 
-        // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void LikeUser([FromBody] Dictionary<string, string> jsonBody)
         {
+            
+
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        [HttpPost]
+        public void DismissUser([FromBody] Dictionary<string, string> jsonBody)
         {
+
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        static async Task Execute()
         {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Sending with Twilio SendGrid is Fun";
+            var to = new EmailAddress("wjwalcher@gmail.com", "Will W");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
         }
+
+
     }
 }
